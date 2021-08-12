66cd8081e63b08922c95c09872719d4b425924fd
Az-s <azim.sa@yahoo.com>
2021-08-12 23:46:31 +0600

    created component (MovieList)

---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import './MovieList.css';
+
+class MovieList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MovieList;
